09f38b4d23199023aed4467fe49b9606d079ca30
ThobekaN <212134676+ThobekaN@users.noreply.github.com>
2026-09-25 00:50:54 +0200

    Update freight_lines.py

--- freight_lines.py
+++ freight_lines.py
@@ -49,7 +49,7 @@ with col_right:
     df_normal = df_log[df_log["Fuel_Litres"] != (df_log.loc[0, "Fuel_Litres"] - 45.0)]
  
 
-    with st.expander(f"🟢 Stable Infrastructure Envelopes ({len(df_normal) - len(df_problem)} Nodes)", expanded=True):
+    with st.expander(f"🟢 Stable Infrastructure Envelopes ({len(df_normal)} - {len(df_problem)} Nodes)", expanded=True):
         if not df_normal.empty:
             for index, row in df_normal.iterrows():
                 st.markdown(f"**🆔 Vehicle ID:** {row['Truck_ID']} | **👤 Operator:** {row['Driver']}")
