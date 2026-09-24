20f7d0268d87e9283d7e8ee482976600ef87f5ec
ThobekaN <212134676+ThobekaN@users.noreply.github.com>
2026-09-25 00:04:31 +0200

    Update freight_lines.py

--- freight_lines.py
+++ freight_lines.py
@@ -62,7 +62,7 @@ with col_right:
     with st.expander(f"🔴 Isolated System Critcal Anomalies ({len(df_normal)} Nodes)", expanded=True):
        if not df_problem.empty:
            for index, row in df_problem.iterrows():
-               st.markdown(f"**🆔 Vehicle ID:** {row['Truck_ID']} | **👤 Operator:** {row['Driver']}"
+               st.markdown(f"**🆔 Vehicle ID:** {row['Truck_ID']} | **👤 Operator:** {row['Driver']}")
                if (df_fleet.loc[0, "Fuel_Litres"] - 45.0) and df_fleet.loc[0, "Speed_KMH"] = 0.0 and row['BURS_Clearance'] == "PROCEED TO BORDER":
                     st.error(f"❌ Critical Telemetry Exposure.")
                     st.success(f"✅ BURS Clearance Approved. Status: **{row['BURS_Clearance']}**")
