8ad7d4002b6d5523aa989908c65e8445ce14ca2e
ThobekaN <212134676+ThobekaN@users.noreply.github.com>
2026-09-25 00:36:13 +0200

    Update freight_lines.py

--- freight_lines.py
+++ freight_lines.py
@@ -72,5 +72,4 @@ with col_right:
 
                    else:
                        st.error("❌ Critical Telemetry Exposure. ⚠️ BURS Clearance Blocked. Status: **{row['BURS_Clearance']}**")
-        else:
-             st.caption("All operational perimeters clear. Zero excursions active")
+
