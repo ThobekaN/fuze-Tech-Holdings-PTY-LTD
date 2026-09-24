1e2a2febd2f40ad382304cc6a54f13ba1ff19578
ThobekaN <212134676+ThobekaN@users.noreply.github.com>
2026-09-24 15:03:34 +0200

    Update main_portal.py

--- main_portal.py
+++ main_portal.py
@@ -185,7 +185,7 @@ def render_home_portal():
                     st.divider()
                     
                     st.markdown("### 📡 API Token Gateway Handshake")
-                    st.caption("Presentation Hint: Paste (`cartrack_oauth2_token_881` || `cartrack_oauth2_token_000`) into the field below.")
+                    st.caption("Presentation Hint: Paste `cartrack_oauth2_token_881 || cartrack_oauth2_token_000` into the field below.")
                     input_log_token = st.text_input("Enter Telematics Provider Read-Token ID", key="tk_logtech")
                     
                     if st.button("Establish API Loop Link", key="btn_connect_logtech"):
